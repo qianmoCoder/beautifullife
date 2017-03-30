807523706c55494ed442f193d2527c8a0c343120
liuhongzhe <wy_liuhongzhe@163.com>
2017-03-30 20:32:15 +0800

    commit

--- icore/src/main/java/com/ddu/icore/dialog/ShareDialogFragment.java
+++ icore/src/main/java/com/ddu/icore/dialog/ShareDialogFragment.java
@@ -3,18 +3,16 @@ package com.ddu.icore.dialog;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.ddu.icore.R;
 import com.ddu.icore.util.ViewUtils;
 
-public class ShareDialogFragment extends DialogFragment implements View.OnClickListener {
+public class ShareDialogFragment extends BottomDialogFragment implements View.OnClickListener {
 
     private TextView mTvCancel;
 
@@ -26,9 +24,6 @@ public class ShareDialogFragment extends DialogFragment implements View.OnClickL
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-        getDialog().getWindow().setBackgroundDrawableResource(android.R.color.transparent);
-
         LinearLayout linearLayout = (LinearLayout) inflater.inflate(R.layout.fragment_share, container, false);
 
         mTvCancel = ViewUtils.findViewById(linearLayout, R.id.tv_cancel);
