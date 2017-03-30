2ee1d5829b9dea0344e2a2a7e138f965f103bbd8
liuhongzhe <wy_liuhongzhe@163.com>
2017-03-30 20:23:55 +0800

    commit

--- icore/src/main/java/com/ddu/icore/dialog/ShareDialogFragment.java
+++ icore/src/main/java/com/ddu/icore/dialog/ShareDialogFragment.java
@@ -3,6 +3,7 @@ package com.ddu.icore.dialog;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +14,7 @@ import android.widget.TextView;
 import com.ddu.icore.R;
 import com.ddu.icore.util.ViewUtils;
 
-public class ShareDialogFragment extends BottomDialogFragment implements View.OnClickListener {
+public class ShareDialogFragment extends DialogFragment implements View.OnClickListener {
 
     private TextView mTvCancel;
 
