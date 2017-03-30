06bc5394ebc9db5e857bc015bd2e972c7741c486
liuhongzhe <wy_liuhongzhe@163.com>
2017-03-30 20:14:47 +0800

    commit

--- icore/src/main/java/com/ddu/icore/navigation/Navigator.java
+++ icore/src/main/java/com/ddu/icore/navigation/Navigator.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 
+import com.ddu.icore.dialog.ShareDialogFragment;
 import com.ddu.icore.ui.activity.ShowDetailActivity;
 
 /**
@@ -21,6 +23,11 @@ public class Navigator {
     public static void startShowDetailActivity(@NonNull Context context, String fragmentName, Bundle bundle) {
         Intent intent = ShowDetailActivity.getShowDetailIntent(context, fragmentName, bundle);
         context.startActivity(intent);
+    }
+
+    public static void startShareFragmentDialog(@NonNull FragmentActivity activity) {
+        ShareDialogFragment shareDialogFragment = ShareDialogFragment.newInstance();
+        shareDialogFragment.show(activity.getSupportFragmentManager(),"shareDialogFragment");
     }
 
 }
