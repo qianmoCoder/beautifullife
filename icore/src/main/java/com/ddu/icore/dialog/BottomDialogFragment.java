5711bd7ce2c8369d7cbdab3bbdf2d39bc127f63d
liuhongzhe <wy_liuhongzhe@163.com>
2017-03-31 10:39:25 +0800

    commit

--- icore/src/main/java/com/ddu/icore/dialog/BottomDialogFragment.java
+++ icore/src/main/java/com/ddu/icore/dialog/BottomDialogFragment.java
@@ -1,9 +1,11 @@
 
 package com.ddu.icore.dialog;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.view.Gravity;
 import android.view.Window;
@@ -12,18 +14,23 @@ import android.view.WindowManager;
 import com.ddu.icore.R;
 
 
-public class BottomDialogFragment extends DialogFragment  {
+public class BottomDialogFragment extends DialogFragment {
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
-        Dialog dialog = new Dialog(getActivity(), getTheme());
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), getTheme());
+
+        Dialog dialog = builder.create();
         dialog.show();
 
         Window window = dialog.getWindow();
-        window.requestFeature(Window.FEATURE_NO_TITLE);
         window.setBackgroundDrawableResource(android.R.color.transparent);
 
         WindowManager.LayoutParams wlp = window.getAttributes();
