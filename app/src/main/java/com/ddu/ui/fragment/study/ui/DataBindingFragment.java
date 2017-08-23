8511eaa98d438d6720b4c35c9f839d42606765db
liuhongzhe <liuhongzhe@etcp.cn>
2017-08-23 11:32:18 +0800

    优化代码

---
+++ app/src/main/java/com/ddu/ui/fragment/study/ui/DataBindingFragment.java
@@ -0,0 +1,27 @@
+package com.ddu.ui.fragment.study.ui;
+
+import android.os.Bundle;
+
+import com.ddu.R;
+import com.ddu.icore.ui.fragment.DefaultFragment;
+
+/**
+ * Created by yzbzz on 2017/8/4.
+ */
+
+public class DataBindingFragment extends DefaultFragment {
+    @Override
+    public int getLayoutId() {
+        return R.layout.fragment_databinding;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData(Bundle savedInstanceState) {
+
+    }
+}
