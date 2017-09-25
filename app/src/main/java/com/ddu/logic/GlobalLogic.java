78bc8e578b3bc55b2de760dd6af3c1d82a6cd006
liuhongzhe <liuhongzhe@etcp.cn>
2017-09-25 14:26:44 +0800

    优化代码

---
+++ app/src/main/java/com/ddu/logic/GlobalLogic.java
@@ -0,0 +1,8 @@
+package com.ddu.logic;
+
+/**
+ * Created by yzbzz on 2017/9/22.
+ */
+
+public class GlobalLogic {
+}
--- app/src/main/java/com/ddu/logic/common/UIBaseLogic.java
+++ app/src/main/java/com/ddu/logic/common/UIBaseLogic.java
@@ -1,24 +1,23 @@
-//package com.ddu.logic.common;
-//
-//
-//import com.ddu.icore.aidl.GodIntent;
-//
-///**
-// * Created by admin on 2016/1/6.
-// */
-//public abstract class UIBaseLogic {
-//
-//    private ObserverManager observerManager;
-//
-//    public UIBaseLogic() {
-//        observerManager = ObserverManager.getInstance();
-//    }
-//
-//    public void notifyUi(final int action, final Object obj, final int stateCode) {
-////        observerManager.notifyUi(action, obj, stateCode);
-//    }
-//
-//    public abstract void initActionListener();
-//
-//    public abstract void onReceiveAction(GodIntent msg);
-//}
+package com.ddu.logic.common;
+
+import com.ddu.icore.aidl.GodIntent;
+import com.ddu.icore.common.ObserverManager;
+
+public abstract class UIBaseLogic {
+
+    private ObserverManager observerManager;
+
+    public UIBaseLogic() {
+        observerManager = ObserverManager.getInstance();
+    }
+
+    public void notifyUi(GodIntent godIntent) {
+        observerManager.notify(godIntent);
+    }
+
+    public abstract void initActionListener();
+
+    public void onReceiveAction(GodIntent msg) {
+
+    }
+}
