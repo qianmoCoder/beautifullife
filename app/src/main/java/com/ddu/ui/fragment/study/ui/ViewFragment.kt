5228356389d135db98cf539a974b322f696a1827
liuhongzhe <liuhongzhe@etcp.cn>
2018-03-01 16:36:34 +0800

    优化代码

--- app/src/main/java/com/ddu/ui/fragment/study/ui/ViewFragment.kt
+++ app/src/main/java/com/ddu/ui/fragment/study/ui/ViewFragment.kt
@@ -1,21 +1,42 @@
 package com.ddu.ui.fragment.study.ui
 
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
-
 import com.ddu.R
 import com.ddu.icore.ui.fragment.DefaultFragment
+import kotlinx.android.synthetic.main.fragment_study_view.*
+import org.jetbrains.anko.horizontalPadding
 
 /**
  * Created by yzbzz on 2017/9/1.
  */
 
 class ViewFragment : DefaultFragment() {
+    var show = false
 
     override fun getLayoutId(): Int {
         return R.layout.fragment_study_view
     }
 
     override fun initView() {
+        var gradientDrawable = GradientDrawable()
+        gradientDrawable.cornerRadius = 4f
+        val color = resources.getColor(R.color.c_252525)
+        gradientDrawable.setStroke(4, color)
+
+        btn_show.setOnClickListener {
+            tv_show.background = if (show) {
+                gradientDrawable
+            } else {
+                null
+            }
+            tv_show.horizontalPadding = if (show) {
+                8
+            } else {
+                0
+            }
+            show = !show
+        }
 
     }
 
