f9602f82d16fdeecd6d37a1c93dbe8a3309c022f
liuhongzhe <liuhongzhe@etcp.cn>
2018-04-04 17:52:32 +0800

    优化代码

---
+++ icore/src/main/java/com/ddu/icore/util/BitmapUtils.kt
@@ -0,0 +1,249 @@
+package com.ddu.icore.util
+
+import android.graphics.*
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.NinePatchDrawable
+import android.media.ExifInterface
+import android.util.Base64
+import android.view.View
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileInputStream
+import java.io.IOException
+
+
+object BitmapUtils {
+
+
+    fun base64ToBitmap(base64Data: String): Bitmap {
+        val bytes = Base64.decode(base64Data, Base64.DEFAULT)
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+    }
+
+
+    fun Bitmap2StrByBase64(bit: Bitmap): String {
+        val bos = ByteArrayOutputStream()
+        bit.compress(Bitmap.CompressFormat.JPEG, 40, bos)
+        val bytes = bos.toByteArray()
+        return Base64.encodeToString(bytes, Base64.DEFAULT)
+    }
+
+    fun Bitmap2Bytes(bit: Bitmap): ByteArray {
+        val bos = ByteArrayOutputStream()
+        bit.compress(Bitmap.CompressFormat.JPEG, 40, bos)
+        return bos.toByteArray()
+    }
+
+    fun calculateInSampleSize(options: BitmapFactory.Options, reqWidth: Float, reqHeight: Float): Int {
+        val height = options.outHeight
+        val width = options.outWidth
+        var inSampleSize = 1
+        if (height > reqHeight || width > reqWidth) {
+            val heightRatio = Math.round(height.toFloat() / reqHeight)
+            val widthRatio = Math.round(width.toFloat() / reqWidth)
+            inSampleSize = if (heightRatio < widthRatio) heightRatio else widthRatio
+        }
+        return inSampleSize
+    }
+
+    fun decodeBGBitmapFromResource(filePath: String): Bitmap {
+        // First decode with inJustDecodeBounds=true to check dimensions
+        val options = BitmapFactory.Options()
+        options.inJustDecodeBounds = true
+        BitmapFactory.decodeFile(filePath, options)
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, 400, 800)
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false
+        options.inPreferredConfig = Bitmap.Config.RGB_565
+        return BitmapFactory.decodeFile(filePath, options)
+    }
+
+    fun decodeSampledBitmapFromResource(filePath: String, reqWidth: Int, reqHeight: Int): Bitmap {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        val options = BitmapFactory.Options()
+        options.inJustDecodeBounds = true
+        BitmapFactory.decodeFile(filePath, options)
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight)
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false
+        return BitmapFactory.decodeFile(filePath, options)
+    }
+
+    fun calculateInSampleSize(options: BitmapFactory.Options, reqWidth: Int, reqHeight: Int): Int {
+        // Raw height and width of image
+        val height = options.outHeight
+        val width = options.outWidth
+        var inSampleSize = 1
+
+        if (height > reqHeight || width > reqWidth) {
+
+            val halfHeight = height / 2
+            val halfWidth = width / 2
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while (halfHeight / inSampleSize > reqHeight && halfWidth / inSampleSize > reqWidth) {
+                inSampleSize *= 2
+            }
+        }
+
+        return inSampleSize
+    }
+
+    fun getBitmapDegree(path: String): Int {
+        var degree = 0
+        try {
+            val exifInterface = ExifInterface(path)
+            val orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)
+            when (orientation) {
+                ExifInterface.ORIENTATION_ROTATE_90 -> degree = 90
+                ExifInterface.ORIENTATION_ROTATE_180 -> degree = 180
+                ExifInterface.ORIENTATION_ROTATE_270 -> degree = 270
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return degree
+    }
+
+    fun rotateBitmapByDegree(bm: Bitmap, degree: Int): Bitmap {
+        var returnBm: Bitmap? = null
+
+        val matrix = Matrix()
+        matrix.postRotate(degree.toFloat())
+        try {
+            returnBm = Bitmap.createBitmap(bm, 0, 0, bm.width, bm.height, matrix, true)
+        } catch (e: OutOfMemoryError) {
+        }
+
+        if (returnBm == null) {
+            returnBm = bm
+        }
+        if (bm != returnBm) {
+            bm.recycle()
+        }
+        return returnBm
+    }
+
+
+    fun drawable2Bitmap(drawable: Drawable): Bitmap? {
+        if (drawable is BitmapDrawable) {
+            return drawable.bitmap
+        } else if (drawable is NinePatchDrawable) {
+            val bitmap = Bitmap
+                    .createBitmap(
+                            drawable.getIntrinsicWidth(),
+                            drawable.getIntrinsicHeight(),
+                            if (drawable.getOpacity() != PixelFormat.OPAQUE)
+                                Bitmap.Config.ARGB_8888
+                            else
+                                Bitmap.Config.RGB_565)
+            val canvas = Canvas(bitmap)
+            drawable.setBounds(0, 0, drawable.getIntrinsicWidth(),
+                    drawable.getIntrinsicHeight())
+            drawable.draw(canvas)
+            return bitmap
+        } else {
+            return null
+        }
+    }
+
+    fun getBitmapFromFile(file: File?, w: Int): Bitmap? {
+        var fileInputStream: FileInputStream? = null
+        try {
+            val opts = BitmapFactory.Options()
+            opts.inJustDecodeBounds = true
+            if (file == null || !file.exists()) {
+                return null
+            } else if (file.length() == 0L) {
+                file.delete()
+                return null
+            }
+            fileInputStream = FileInputStream(file)
+            BitmapFactory.decodeFile(file.absolutePath, opts)
+            val be = getSampleSize(opts.outWidth.toFloat(), w.toFloat())
+            opts.inSampleSize = be
+            opts.inJustDecodeBounds = false
+            opts.inPreferredConfig = Bitmap.Config.ARGB_8888
+            return BitmapFactory.decodeStream(fileInputStream, null, opts)
+
+
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } finally {
+            if (fileInputStream != null) {
+                try {
+                    fileInputStream.close()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+
+            }
+        }
+        return null
+    }
+
+    fun getSampleSize(size: Float, outSize: Float): Int {
+        var be = (size / outSize).toInt()
+        if (be <= 0) {
+            be = 1
+        }
+        return be
+
+    }
+
+    fun getMatrixBitmap(bm: Bitmap?, w: Int, h: Int, needRecycle: Boolean): Bitmap {
+        val width = bm!!.width
+        val height = bm.height
+        val isCompress = width > w && height > h && w != 0 && h != 0
+        if (isCompress) {
+            val scaleWidth = w.toFloat() / width
+            val scaleHeight = h.toFloat() / height
+            val scale = Math.max(scaleWidth, scaleHeight)
+            val matrix = Matrix()
+            matrix.postScale(scale, scale)
+            val bitmap = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, true)
+            if (needRecycle && bm != null && bm != bitmap) {
+                bm.recycle()
+            }
+            return bitmap
+        } else {
+            return bm
+        }
+
+    }
+
+    fun bigBitmap(bitmap: Bitmap): Bitmap {
+        val matrix = Matrix()
+        matrix.postScale(1.2f, 1.2f) //长和宽放大缩小的比例
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+    }
+
+
+    fun getViewBitmap(view: View): Bitmap {
+
+        view.measure(
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED))
+        view.layout(0, 0,
+                view.measuredWidth,
+                view.measuredHeight)
+
+        view.buildDrawingCache()
+        val cacheBitmap = view.drawingCache
+        val bitmap = Bitmap.createBitmap(cacheBitmap)
+        view.destroyDrawingCache()
+
+        return bitmap
+    }
+
+}
