3fc2d5509d10256de6695d2186b9df41dd26b7d7
liuhongzhe <liuhongzhe@etcp.cn>
2018-08-15 18:28:26 +0800

    优化UI

--- icore/src/main/java/com/ddu/icore/navigation/IRouter.java
+++
@@ -1,8 +0,0 @@
-package com.ddu.icore.navigation;
-
-/**
- * Created by yzbzz on 2018/2/12.
- */
-
-public class IRouter {
-}
--- icore/src/main/java/com/ddu/icore/rx/pay/RxPayResultInfo.kt
+++
@@ -1,8 +0,0 @@
-package com.ddu.icore.rx.pay
-
-
-/**
- * Created by yzbzz on 2018/1/5.
- */
-
-data class RxPayResultInfo(val isSuccess: Boolean = false)
