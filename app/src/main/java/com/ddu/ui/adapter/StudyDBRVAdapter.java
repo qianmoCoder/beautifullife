df9466208e835b4b18aebdc7ec7e105c55915ce7
liuhongzhe <liuhongzhe@etcp.cn>
2018-11-12 14:29:22 +0800

    优化代码

--- app/src/main/java/com/ddu/ui/adapter/StudyDBRVAdapter.java
+++ app/src/main/java/com/ddu/ui/adapter/StudyDBRVAdapter.java
@@ -1,7 +1,6 @@
 package com.ddu.ui.adapter;
 
 import android.databinding.DataBindingUtil;
-import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.recyclerview.extensions.ListAdapter;
 import android.support.v7.widget.RecyclerView;
@@ -18,57 +17,26 @@ import com.iannotation.model.RouteMeta;
 public class StudyDBRVAdapter extends ListAdapter<RouteMeta, StudyDBRVAdapter.ViewHolder> {
 
     private Consumer1<RouteMeta> consumer1;
-    private int radius;
 
     public StudyDBRVAdapter() {
         super(new StudyDiffCallback());
     }
 
-//    public StudyDBRVAdapter(Context context, List<RouteMeta> items) {
-//        super(context, items);
-//        radius = context.getResources().getDimensionPixelSize(R.dimen.dp_5);
-//    }
-//
-//    @Override
-//    public int getLayoutId(int viewType) {
-//        return R.layout.fragment_study_rv_item;
-//    }
-//
-//    @Override
-//    public void bindView(ViewHolder viewHolder, final RouteMeta data, final int position) {
-//        TextView tvTitle = viewHolder.getView(R.id.tv_title);
-//        tvTitle.setText(data.getText());
-//
-//        ShapeInject.inject(tvTitle)
-//                .setRadius(radius)
-//                .setBackgroundColor(getColor(data.getColor()))
-//                .background();
-//
-//        viewHolder.setText(R.id.tv_description, data.getDescription());
-//        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
-//
-//            @Override
-//            public void onClick(View v) {
-//                if (null != consumer1) {
-//                    consumer1.accept(data);
-//                }
-//            }
-//        });
-//    }
-
-    private int getColor(String colorStr) {
-        int color;
-        try {
-            color = Color.parseColor(colorStr);
-        } catch (Exception e) {
-            color = Color.BLUE;
-        }
-        return color;
-    }
-
 
     public void setItemClickListener(Consumer1<RouteMeta> consumer1) {
         this.consumer1 = consumer1;
+    }
+
+    public View.OnClickListener createOnClickListener(final RouteMeta routeMeta) {
+        return new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (consumer1 != null) {
+                    consumer1.accept(routeMeta);
+                }
+            }
+        };
     }
 
     @NonNull
@@ -82,7 +50,8 @@ public class StudyDBRVAdapter extends ListAdapter<RouteMeta, StudyDBRVAdapter.Vi
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         final RouteMeta routeMeta = getItem(position);
         holder.itemView.setTag(routeMeta);
-        holder.bind(routeMeta);
+
+        holder.bind(createOnClickListener(routeMeta), routeMeta);
         if (consumer1 != null) {
             holder.itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -102,7 +71,7 @@ public class StudyDBRVAdapter extends ListAdapter<RouteMeta, StudyDBRVAdapter.Vi
             this.binding = binding;
         }
 
-        public void bind(RouteMeta routeMeta) {
+        public void bind(View.OnClickListener clickListener, RouteMeta routeMeta) {
             StudyViewModel studyViewModel = new StudyViewModel(itemView.getContext(), routeMeta);
             binding.setViewModel(studyViewModel);
             binding.executePendingBindings();
