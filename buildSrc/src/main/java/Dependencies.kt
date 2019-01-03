2264e6e43ca9a16ab5e41128ca6d2fbe9dfa025c
liuhongzhe <liuhongzhe@etcp.cn>
2019-01-03 17:02:11 +0800

    优化代码

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,18 @@
+object Versions {
+
+    val compileSdkVersion = 28
+
+    val minSdkVersion = 19
+    val targetSdkVersion = 28
+
+    val versionCode = 1
+    val versionName = "1.0"
+
+    val kotlin_version = "1.3.11"
+
+    val supportLibraryVersion = "27.1.0"
+}
+
+object Libs {
+    val fastjson = "com.alibaba:fastjson:1.1.68.android"
+}
