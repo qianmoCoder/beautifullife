e6bd21dfe06bd9731eee9d82114b3ca097092592
liuhongzhe <liuhongzhe@etcp.cn>
2019-08-16 21:28:38 +0800

    优化代码

--- icore/src/main/java/com/ddu/icore/common/Executors.kt
+++ icore/src/main/java/com/ddu/icore/common/Executors.kt
@@ -1,9 +1,9 @@
 package com.ddu.icore.common
 
+import android.os.Handler
+import android.os.Looper
+import java.util.concurrent.*
 import java.util.concurrent.Executors
-import java.util.concurrent.LinkedBlockingQueue
-import java.util.concurrent.ThreadPoolExecutor
-import java.util.concurrent.TimeUnit
 import kotlin.math.max
 import kotlin.math.min
 
@@ -12,16 +12,19 @@ import kotlin.math.min
  */
 object Executors {
 
-    private val CPU_COUNT = Runtime.getRuntime().availableProcessors()
-    private val CORE_POOL_SIZE = max(2, min(CPU_COUNT - 1, 4))
-    private val MAXIMUM_POOL_SIZE = CPU_COUNT * 2 + 1
-    private const val KEEP_ALIVE_SECONDS = 30L
-    private val sPoolWorkQueue = LinkedBlockingQueue<Runnable>(128)
+    val CPU_COUNT = Runtime.getRuntime().availableProcessors()
+    val CORE_POOL_SIZE = max(2, min(CPU_COUNT - 1, 4))
+    val MAXIMUM_POOL_SIZE = CPU_COUNT * 2 + 1
+    const val KEEP_ALIVE_SECONDS = 30L
+    val sPoolWorkQueue = LinkedBlockingQueue<Runnable>(128)
 
-    private val IO_EXECUTOR = Executors.newSingleThreadExecutor()
-    private var THREAD_POOL_EXECUTOR: ThreadPoolExecutor
+    val DISK_IO_EXECUTOR = Executors.newSingleThreadExecutor()
+    val NETWORK_IO_EXECUTOR = Executors.newFixedThreadPool(3)
+    val MAIN_THREAD_EXECUTOR = MainThreadExecutor()
 
-    private val IO_SCHEDULED_EXECUTOR = Executors.newSingleThreadScheduledExecutor()
+    val SCHEDULED_IO__EXECUTOR = Executors.newSingleThreadScheduledExecutor()
+
+    var THREAD_POOL_EXECUTOR: ThreadPoolExecutor
 
     init {
         val threadPoolExecutor = ThreadPoolExecutor(
@@ -32,14 +35,25 @@ object Executors {
         THREAD_POOL_EXECUTOR = threadPoolExecutor
     }
 
-    fun ioThread(f: () -> Unit) = IO_EXECUTOR.execute(f)
+    fun ioThread(f: () -> Unit) = DISK_IO_EXECUTOR.execute(f)
+
+    fun netWorkIoThread(f: () -> Unit) = NETWORK_IO_EXECUTOR.execute(f)
+
+    fun mainThread(f: () -> Unit) = MAIN_THREAD_EXECUTOR.execute(f)
 
     fun excute(f: () -> Unit) = THREAD_POOL_EXECUTOR.execute(f)
 
-    fun schedule(f: () -> Unit, delay: Long, unit: TimeUnit) = IO_SCHEDULED_EXECUTOR.schedule(f, delay, unit)
+    fun schedule(f: () -> Unit, delay: Long, unit: TimeUnit) = SCHEDULED_IO__EXECUTOR.schedule(f, delay, unit)
 
     fun scheduleAtFixedRat(f: () -> Unit, initialDelay: Long, period: Long, unit: TimeUnit) =
-        IO_SCHEDULED_EXECUTOR.scheduleAtFixedRate(f, initialDelay, period, unit)
+        SCHEDULED_IO__EXECUTOR.scheduleAtFixedRate(f, initialDelay, period, unit)
 
+    class MainThreadExecutor : Executor {
+        val handler = Handler(Looper.getMainLooper())
+
+        override fun execute(command: Runnable?) {
+            handler.post(command)
+        }
+    }
 }
 
